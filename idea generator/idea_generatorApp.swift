6eeacba8a673618daa41c5cdf4cd9e5645a78ea7
xy <xinyipayxy@gmail.com>
2022-10-22 09:27:07 +0800

    Initial Commit

---
+++ idea generator/idea_generatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  idea_generatorApp.swift
+//  idea generator
+//
+//  Created by T Krobot on 22/10/22.
+//
+
+import SwiftUI
+
+@main
+struct idea_generatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
