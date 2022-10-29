245f4efe5943b3de0d10c70b87654a95855675b6
Kai Qi <kaiqilee28@gmail.com>
2022-10-29 09:28:31 +0800

    changed saved button to an image but still does not work

--- ActivityDescriptionView.swift
+++ ActivityDescriptionView.swift
@@ -8,23 +8,24 @@
 import SwiftUI
 
 struct ActivityDescriptionView: View {
-    var num = 0 // need to figure out how to transfer the num in generatorView to here
+    var num = 0
+    // need to figure out how to transfer the num in generatorView to here
     
     var body: some View {
         VStack {
-            Spacer()
             HStack {
+                Spacer()
                 Text(placesToGo[num].title)
                     .font(.title)
                     .bold()
                 Spacer()
-                Button {
-                    placesToGo[num].isSaved.toggle() //doesnt work
-                } label: {
-                    Image(systemName: placesToGo[num].isSaved ?  "bookmark.fill" : "bookmark")
-                        .foregroundColor(.gray)
-                        .font(.title)
-                }
+                Image(systemName: placesToGo[num].isSaved ?  "bookmark.fill" : "bookmark")
+                    .foregroundColor(.gray)
+                    .font(.title)
+                    .onTapGesture {
+                        placesToGo[num].isSaved.toggle()
+                    }
+                    
             }
             .padding(25)
             
