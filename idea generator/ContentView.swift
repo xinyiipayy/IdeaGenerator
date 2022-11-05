252fe7eaabab27e4211c577437cc74d9f41701fe
Kai Qi <kaiqilee28@gmail.com>
2022-11-05 10:18:38 +0800

    created the array of places to go in content view

--- idea generator/ContentView.swift
+++ idea generator/ContentView.swift
@@ -9,6 +9,21 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State var placesToGo = [Place(title: "Gardens By The Bay",
+                                   description: "One of Asia's premier horticultural destinations, Gardens by the Bay offers a scenic paradise for nature and photography lovers, as well as the whole family.",
+                                   address: "18 Marina Gardens Dr, Singapore 018953",
+                                   openingHours: "5am - 2am",
+                                   link: "https://ticket.gardensbythebay.com.sg "),
+                             Place(title: "Universal Studios Singapore",
+                                   description: "Universal Studios Singapore is a theme park located within the Resorts World Sentosa at Sentosa, Singapore. It features 28 rides, shows, and attractions in seven themed zones. It is one of the six Universal Studios theme parks around the world.",
+                                   address: "8 Sentosa Gateway, 098269",
+                                   openingHours: "11am - 6pm, 7:30pm - 12:30am",
+                                   link: "https://www.rwsentosa.com/en/attractions/universal-studios-singapore "),
+                             Place(title: "Jewel Changi Airport",
+                                   description: "Lush, 10-floor retail complex with hundreds of shops & eateries, indoor gardens & a 40-m waterfall",
+                                   address: "78 Airport Blvd., Singapore 819666",
+                                   openingHours: "Open 24 hours",
+                                   link: "https://www.jewelchangiairport.com/en/ticketing.html ")]
     
     var body: some View {
         TabView {
