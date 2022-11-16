6d81b5c87cd15db1dc4cebc93fefbe2e16a96e19
katelyn <katelynkang28@gmail.com>
2022-11-16 15:16:00 +0800

    added a dropdown thing to the saved view

--- idea generator/SavedView.swift
+++ idea generator/SavedView.swift
@@ -17,6 +17,10 @@ struct SavedView: View {
     @State var cat: Category = .place
     @State var isSheetShown = false
     
+    @State var showCrafts = true
+    @State var showSports = true
+    @State var showPlaces = true
+    
     var body: some View {
         
         ScrollView {
@@ -30,117 +34,151 @@ struct SavedView: View {
                 
                 VStack {
                     HStack {
+                        Image(systemName: "triangle")
+                            .rotationEffect(showCrafts ? Angle(degrees: 180) : Angle(degrees: 90))
+                            .onTapGesture {
+                                withAnimation {
+                                    showCrafts.toggle()
+                                }
+                            }
                         Text("Arts & Craft")
                             .bold()
                             .font(.headline)
                         Spacer()
                     }
-                    if crafts.filter { $0.isSaved }.count == 0 {
-                        Text("There are no saved ideas for this category.")
-                            .foregroundColor(.gray)
-                            .padding(5)
-                    } else {
-                        ForEach(crafts) { craft in
-                            if craft.isSaved == true {
-                                Button {
-                                    cat = .craft
-                                    num = 0
-                                    while crafts[num].title != craft.title {
-                                        num += 1
+                    if showCrafts == true {
+                        if crafts.filter { $0.isSaved }.count == 0 {
+                            Text("There are no saved ideas for this category.")
+                                .foregroundColor(.gray)
+                                .padding(5)
+                        } else if showCrafts == true {
+                            ForEach(crafts) { craft in
+                                if craft.isSaved == true {
+                                    Button {
+                                        cat = .craft
+                                        num = 0
+                                        while crafts[num].title != craft.title {
+                                            num += 1
+                                        }
+                                        isSheetShown = true
+                                    } label: {
+                                        Text(craft.title)
+                                            .bold()
+                                            .padding()
+                                            .frame(width: 350, height: 55)
+                                            .background(Color("lightYellow"))
+                                            .cornerRadius(10)
+                                            .foregroundColor(Color("darkYellow"))
                                     }
-                                    isSheetShown = true
-                                } label: {
-                                    Text(craft.title)
-                                        .bold()
-                                        .padding()
-                                        .frame(width: 350, height: 55)
-                                        .background(Color("lightYellow"))
-                                        .cornerRadius(10)
-                                        .foregroundColor(Color("darkYellow"))
+                                } else {
+                                    EmptyView()
                                 }
-                            } else {
-                                EmptyView()
                             }
                         }
+                    } else {
+                        EmptyView()
                     }
                 }
                 .padding(10)
                 
                 VStack {
                     HStack {
+                        Image(systemName: "triangle")
+                            .rotationEffect(showSports ? Angle(degrees: 180) : Angle(degrees: 90))
+                            .onTapGesture {
+                                withAnimation {
+                                    showSports.toggle()
+                                }
+                            }
                         Text("Sports")
                             .bold()
                             .font(.headline)
                         Spacer()
                     }
-                    if sports.filter { $0.isSaved }.count == 0 {
-                        Text("There are no saved ideas for this category.")
-                            .foregroundColor(.gray)
-                            .padding(5)
-                    } else {
-                        ForEach(sports) { sport in
-                            if sport.isSaved == true {
-                                Button {
-                                    cat = .sport
-                                    num = 0
-                                    while sports[num].title != sport.title {
-                                        num += 1
+                    if showSports == true {
+                        if sports.filter { $0.isSaved }.count == 0 {
+                            Text("There are no saved ideas for this category.")
+                                .foregroundColor(.gray)
+                                .padding(5)
+                        } else {
+                            ForEach(sports) { sport in
+                                if sport.isSaved == true {
+                                    Button {
+                                        cat = .sport
+                                        num = 0
+                                        while sports[num].title != sport.title {
+                                            num += 1
+                                        }
+                                        isSheetShown = true
+                                    } label: {
+                                        Text(sport.title)
+                                            .bold()
+                                            .padding()
+                                            .frame(width: 350, height: 55)
+                                            .background(Color("lightBlue"))
+                                            .cornerRadius(10)
+                                            .foregroundColor(Color("darkBlue"))
                                     }
-                                    isSheetShown = true
-                                } label: {
-                                    Text(sport.title)
-                                        .bold()
-                                        .padding()
-                                        .frame(width: 350, height: 55)
-                                        .background(Color("lightBlue"))
-                                        .cornerRadius(10)
-                                        .foregroundColor(Color("darkBlue"))
+                                } else {
+                                    EmptyView()
                                 }
-                                
-                            } else {
-                                EmptyView()
                             }
                         }
+                    } else {
+                        EmptyView()
                     }
                 }
                 .padding(10)
                 
                 VStack {
                     HStack {
+                        Image(systemName: "triangle")
+                            .rotationEffect(showPlaces ? Angle(degrees: 180) : Angle(degrees: 90))
+                            .onTapGesture {
+                                withAnimation {
+                                    showPlaces.toggle()
+                                }
+                            }
                         Text("Places to Go")
                             .bold()
                             .font(.headline)
                         Spacer()
                     }
-                    if placesToGo.filter { $0.isSaved }.count == 0 {
-                        Text("There are no saved ideas for this category.")
-                            .foregroundColor(.gray)
-                            .padding(5)
-                    } else {
-                        ForEach(placesToGo) { placeToGo in
-                            if placeToGo.isSaved == true {
-                                Button {
-                                    cat = .place
-                                    num = 0
-                                    while placesToGo[num].title != placeToGo.title {
-                                        num += 1
+                    if showPlaces == true {
+                        if placesToGo.filter { $0.isSaved }.count == 0 {
+                            Text("There are no saved ideas for this category.")
+                                .foregroundColor(.gray)
+                                .padding(5)
+                        } else {
+                            ForEach(placesToGo) { placeToGo in
+                                if placeToGo.isSaved == true {
+                                    Button {
+                                        cat = .place
+                                        num = 0
+                                        while placesToGo[num].title != placeToGo.title {
+                                            num += 1
+                                        }
+                                        isSheetShown = true
+                                    } label: {
+                                        Text(placeToGo.title)
+                                            .bold()
+                                            .padding()
+                                            .frame(width: 350, height: 55)
+                                            .background(Color("lightRed"))
+                                            .cornerRadius(10)
+                                            .foregroundColor(Color("darkRed"))
                                     }
-                                    isSheetShown = true
-                                } label: {
-                                    Text(placeToGo.title)
-                                        .bold()
-                                        .padding()
-                                        .frame(width: 350, height: 55)
-                                        .background(Color("lightRed"))
-                                        .cornerRadius(10)
-                                        .foregroundColor(Color("darkRed"))
+                                    
+                                } else {
+                                    EmptyView()
                                 }
-                                
-                            } else {
-                                EmptyView()
                             }
                         }
                     }
+                    else {
+                        EmptyView()
+                    }
+                    
                 }
                 .padding(10)
                 
